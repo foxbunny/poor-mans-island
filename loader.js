fc1b783be7f3bd277b00df608bc087bee8a95d2f
Hajime Yamasaki Vukelic <hi@devi.us>
2023-03-26 13:43:55 +0200

    Remove premature optimization

--- loader.js
+++ loader.js
@@ -1,15 +1,16 @@
 {
-  let
-    $d = document,
-    load = $ => $d.head.append(Object.assign($d.createElement('script'), {async: 1, src: `components/${$.tagName.toLowerCase()}.js`})),
-    io = new IntersectionObserver(es => {
-      for (let e of es)
-        if (e.isIntersecting) {
-          io.unobserve(e.target)
-          load(e.target)
-        }
-    })
-  requestIdleCallback(() => {
-    for (let $ of $d.querySelectorAll('*')) if ($.tagName.includes('-')) io.observe($)
+let
+  $d = document,
+  load = $ => $d.head.append(Object.assign(
+    $d.createElement('script'),
+    {async: 1, src: $.dataset.src || `components/${$.tagName.toLowerCase()}.js`}
+  )),
+  io = new IntersectionObserver(es => {
+    for (let e of es)
+      if (e.isIntersecting) {
+        io.unobserve(e.target)
+        load(e.target)
+      }
   })
+for (let $ of $d.querySelectorAll('*')) if ($.tagName.includes('-')) io.observe($)
 }
