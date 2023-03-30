8789ed3fc36c3ead688382bcace5b76b302465b7
Hajime Yamasaki Vukelic <hi@devi.us>
2023-03-31 01:02:37 +0200

    Updated loader

--- loader.js
+++ loader.js
@@ -1,16 +1,11 @@
 {
-let
-  $d = document,
-  load = $ => $d.head.append(Object.assign(
-    $d.createElement('script'),
-    {async: 1, src: $.dataset.src || `components/${$.tagName.toLowerCase()}.js`}
-  )),
-  io = new IntersectionObserver(es => {
-    for (let e of es)
-      if (e.isIntersecting) {
-        io.unobserve(e.target)
-        load(e.target)
-      }
-  })
-for (let $ of $d.querySelectorAll('*')) if ($.tagName.includes('-')) io.observe($)
+let load = $ => import($.dataset.module || `/components/${$.tagName.toLowerCase()}.js`)
+let io = new IntersectionObserver(es => {
+  for (let e of es) if (e.isIntersecting) {
+    io.unobserve(e.target)
+    load(e.target)
+  }
+})
+for (let $ of document.querySelectorAll('*'))
+  if ($.tagName.includes('-')) io.observe($)
 }
