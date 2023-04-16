043054afbd2449102482f61c13da6ac917d5f413
Hajime Yamasaki Vukelic <hi@devi.us>
2023-04-16 15:04:43 +0200

    Simplify loader

--- loader.js
+++ loader.js
@@ -1,11 +1,9 @@
 {
-let load = $ => import($.dataset.module || `/components/${$.tagName.toLowerCase()}.js`)
 let io = new IntersectionObserver(es => {
   for (let e of es) if (e.isIntersecting) {
     io.unobserve(e.target)
-    load(e.target)
+    import(e.target.dataset.module || `/components/${e.target.tagName.toLowerCase()}.js`)
   }
 })
-for (let $ of document.querySelectorAll('*'))
-  if ($.tagName.includes('-')) io.observe($)
+for (let $ of document.querySelectorAll('*')) if ($.tagName.includes('-')) io.observe($)
 }
